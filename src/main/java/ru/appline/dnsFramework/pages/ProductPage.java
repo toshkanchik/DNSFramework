66b3e081c94ad94f7c5c67205e17b53673e14265
AntonL <toshkanchik@gmail.com>
2021-02-10 17:54:56 +0300

    Поменял sleep на корректные проверки

--- src/main/java/ru/appline/dnsFramework/pages/ProductPage.java
+++ src/main/java/ru/appline/dnsFramework/pages/ProductPage.java
@@ -3,9 +3,12 @@ package ru.appline.dnsFramework.pages;
 import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import ru.appline.dnsFramework.utils.Product;
 import ru.appline.dnsFramework.utils.warrantyEnum;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.textToBePresentInElement;
 
 public class ProductPage extends BasePage{
     @FindBy(xpath = "//h1[@class='page-title product-card-top__title']")
@@ -24,17 +27,14 @@ public class ProductPage extends BasePage{
     }
 
     public ProductPage chooseWarranty(warrantyEnum val) {
+        String tmp = price.getText();
         Select selector = new Select(warranty);
         switch(val) {
             case NONE: selector.selectByValue("default");
             case ONE_YEAR: selector.selectByValue("0");
             case TWO_YEARS: selector.selectByValue("1");
         }
-        try {//приходится делать именно слипом, что-бы метод был универсальным (при переключении с 1 года на 2 не меняется ничего, кроме цены)
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        wait.until(ExpectedConditions.not(textToBePresentInElement(price, tmp)));
         return pages.getProductPage();
     }
 
@@ -42,12 +42,7 @@ public class ProductPage extends BasePage{
         Assertions.assertEquals(buttonBuy.getText(),"Купить", "Убедитесь, что такого товара нет в корзине");
         elementToBeClickable(buttonBuy);
         buttonBuy.click();
-        try {//для проверки успешного клика необходимо подождать, пока текст в кнопке не поменяется
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        Assertions.assertEquals(buttonBuy.getText(),"В корзине", "При добавлении в корзину произошла ошибка");
+        wait.until(ExpectedConditions.textToBePresentInElement(buttonBuy, "В корзине"));
         return pages.getProductPage();
     }
 }
